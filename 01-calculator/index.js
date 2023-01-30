bc2e7be3a88089ab14aa68ff9b9f92be7fc3823d
joeyclapton <joeyclapton42@gmail.com>
2023-01-30 07:24:43 -0300

    feat: add sum function

---
+++ 01-calculator/index.js
@@ -0,0 +1,9 @@
+var sum = require("./sum");
+var div = require("./divide");
+var mult = require("./multiply");
+var sub = require("./subtract");
+
+console.log(sum(2, 3));
+console.log(div(20, 2));
+console.log(mult(90, 3));
+console.log(sub(30, 99));
