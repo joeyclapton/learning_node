71b6954fc2cf9b09f8f061a538031f7b272204ac
joeyclapton <joeyclapton42@gmail.com>
2023-01-31 20:06:35 -0300

    chore: remove sequelize and add pure mysql

--- 02-http/index.js
+++ 02-http/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
-
+const mysql = require("mysql");
 const app = express();
 
 app.get("/", (req, res) => {
   const { name } = req.query;
   const showName = name ? ", " + name : "";
+
+  console.log(mysql);
 
   res.send(`Good morning${showName}`);
 });
