06aa8c837c40fc6aa6c75231b593f8753bae170d
joeyclapton <joeyclapton42@gmail.com>
2023-01-31 21:05:58 -0300

    chore: install handlebars express

--- 02-http/index.js
+++ 02-http/index.js
@@ -1,14 +1,4 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  const { name } = req.query;
-  const showName = name ? ", " + name : "";
-  res.send(`Good morning${showName}`);
-});
-
-app.get("/about", (req, res) => {
-  res.sendFile(__dirname + "/templates/greetings.html");
-});
-
 app.listen(8080);
